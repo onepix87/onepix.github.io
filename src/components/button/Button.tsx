9ac47564ad40eea9566fd770b0568eee98962c99
onepix87 <yavk87@gmail.com>
2024-12-23 18:59:40 +0400

    Build test

--- src/components/button/Button.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import './Button.css';
-
-export const Button: React.FC<{ type?: 'transparent' | 'contrast'; className?: string }> = ({ type = 'transparent', className, children }) => (
-    <button className={`button ${type} ${className ?? ''}`}>
-        {children}
-    </button>
-)
--- src/components/text/index.ts
+++
@@ -1,1 +0,0 @@
-export { Title, Text } from './Text';
